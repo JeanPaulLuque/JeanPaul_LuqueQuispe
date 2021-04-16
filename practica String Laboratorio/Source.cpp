5de933203ef81caaa87c9045e8cc5c41cb33ce54
JeanPaulLuque <46849915+JeanPaulLuque@users.noreply.github.com>
2021-04-16 14:22:39 -0400

    Add files via upload

---
+++ practica String Laboratorio/Source.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+int divisionEnteros(int a, int n);
+int main()
+{
+	string cadena1 = "Intente analizar mas profundamente los mensajes recibidos";
+	string resultado;
+	vector <string> palabras;
+	int cadaBloque = 4;
+	int resto = cadena1.length() % cadaBloque;
+	
+	if (cadena1.length() % cadaBloque)//caracteres faltantees
+	{
+		string adicional(cadaBloque - resto, 'x');
+		cadena1.append(adicional);
+	}
+	
+	cout << "PASO C:\n";
+	for (int i = 0; i < cadena1.length(); i += cadaBloque)
+	{
+		string out = cadena1.substr(i, 4);
+		cout<<out<<endl;//paso c
+		resultado.append(out+"*");
+		palabras.push_back(out);
+	}
+	cout << "\nPASO E:\n"<<resultado<<endl;// resultado almacena el paso e
+	cout << "\nPASO G : \n" << endl;
+	int cont = 0;
+	for (auto i = palabras.begin(); i != palabras.end(); i++,cont++)
+	{
+		cout << "pos :" <<cont<<" bloque: "<<*i<< endl;
+	}
+	return 0;
+}
